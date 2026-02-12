50105e30dd61b5df930008f6a945f088c14a524c
kafkova7-max <kafkova7@gmail.com>
2026-02-12 14:25:53 +0100

    Delete api/index.js

--- api/index.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  res.setHeader('Content-Type', 'application/json');
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  // Odpoví na jakýkoliv dotaz tímto klíčem
-  res.status(200).send({
-    Key: "aazcbp9Jy8jLV0T7psc8eR9CV3AJ2U"
-  });
-}
