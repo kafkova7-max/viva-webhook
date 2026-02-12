681cfc70ba8a3a6396e2d95e8a5c36ec01975bb0
kafkova7-max <kafkova7@gmail.com>
2026-02-12 13:19:08 +0100

    hotovo

--- api/viva.js
+++ api/viva.js
@@ -1,8 +1,11 @@
 export default function handler(req, res) {
-  res.setHeader('Content-Type', 'application/json');
+  // Povolíme přístup robota Vivy
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Content-Type', 'application/json');
 
-  res.status(200).json({
-    Key: "aazcbp9Jy8jLV0T7psc8eR9CV3AJ2U"
-  });
+  // Klíč musíme poslat jako prostý text v JSON formátu
+  const response = { Key: "aazcbp9Jy8jLV0T7psc8eR9CV3AJ2U" };
+  
+  return res.status(200).end(JSON.stringify(response));
 }
