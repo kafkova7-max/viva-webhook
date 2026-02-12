dc2d7b603aa1992ccbfefbe23d6efb435f5e5fb9
kafkova7-max <kafkova7@gmail.com>
2026-02-12 14:21:31 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,17 +1,8 @@
 export default function handler(req, res) {
-  // Povolíme všechno, co by Viva mohla chtít
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('Content-Type', 'application/json');
-
-  // Pokud Viva jen "zkouší" spojení (OPTIONS), hned jí odpovíme OK
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end();
-  }
-
-  // Pošleme klíč v čistém JSON formátu
-  res.status(200).json({
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // Odpoví na jakýkoliv dotaz tímto klíčem
+  res.status(200).send({
     Key: "aazcbp9Jy8jLV0T7psc8eR9CV3AJ2U"
   });
 }
