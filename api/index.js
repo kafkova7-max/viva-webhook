13b2dccd04dd2ed1efcfbfa085dd28ffaf7e913e
kafkova7-max <kafkova7@gmail.com>
2026-02-12 18:52:00 +0100

    Create index.js

---
+++ api/index.js
@@ -0,0 +1,7 @@
+export default function handler(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.status(200).send({
+    Key: "aazcbp9Jy8jLV0T7psc8eR9CV3AJ2U"
+  });
+}
